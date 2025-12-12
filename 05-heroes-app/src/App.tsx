0d386f4d74ed9051147146b8e4d05d1a7a54bb5f
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-12-11 20:31:10 -0500

    feat(ui): agrega componentes de UI

--- 05-heroes-app/src/App.tsx
+++ 05-heroes-app/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components";
+
 export const App = () => {
     return (
         <>
             <h1>Heroes App</h1>
+            <Button
+            variant="default"
+            >Click</Button>
         </>
     );
 };
