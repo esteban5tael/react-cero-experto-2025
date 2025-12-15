6df096db1f38f270494168281c18a96c6630b4f9
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-12-15 07:06:40 -0500

    feat(ui): agrega fuente Montserrat y contenido de bienvenida

--- 07-teslo-shop/front-teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/front-teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,7 +1,12 @@
 export const HomePage = () => {
     return (
         <>
-            <div>HomePage</div>
+            <h1>Hola Mundo!</h1>
+            <h1 className="font-montserrat">Hola Mundo!</h1>
+            <h1 className="font-montserrat font-thin">Hola Mundo!</h1>
+            <h1 className="font-montserrat font-normal">Hola Mundo!</h1>
+            <h1 className="font-montserrat font-bold">Hola Mundo!</h1>
+            
         </>
     );
 };
